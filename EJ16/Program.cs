53cf0579abb9163f83d8cad14c962086fdf76d07
Gaston Milano <gastonmilano15@gmail.com>
2018-08-16 15:23:31 -0300

    Falta comentar solo el ejercicio 13

--- EJ16/Program.cs
+++ EJ16/Program.cs
@@ -10,48 +10,48 @@ namespace EJ16
     {
         static void Main(string[] args)
         {
-            //Se pide al usuario que ingrese una de las palabras a evaluar
-            Console.WriteLine("Ingrese la primer palabra: ");
-            Console.WriteLine("");
+            //Se pide al usuario que ingrese una de las palabras a evaluar.
+            Console.Write("Ingrese la primer palabra: ");
             string iPalabra1 = Console.ReadLine();
 
-            //Se pide al usuario que ingrese la siguiente palabra a evaluar
-            Console.WriteLine("Ingrese la segunda palabra: ");
-            Console.WriteLine("");
+            //Se pide al usuario que ingrese la siguiente palabra a evaluar.
+            Console.Write("Ingrese la segunda palabra: ");
             string iPalabra2 = Console.ReadLine();
 
-            bool iAnagrama = false;
+            Console.WriteLine("");
+
+            bool iCoincidencia = false;
 
             int i = 0;
 
-            if (iPalabra1.Length == iPalabra2.Length)
-            {
-                while ((i + 1) <= iPalabra1.Length)
+            if (iPalabra1.Length == iPalabra2.Length)               // Primero nos fijamos si las palabras son del mismo size,             
+            {                                                       // si no lo son, no son anagramas.
+                while ((i + 1) <= iPalabra1.Length)                 
                 {
-                    iAnagrama = false;
+                    iCoincidencia = false;                              
 
-                    for (int j = 0; (j < iPalabra2.Length) && (iAnagrama == false); j++)
-                    {
-                        if (iPalabra1[i] == iPalabra2[j])
-                        {
+                    for (int j = 0; (j < iPalabra2.Length) && (iCoincidencia == false); j++)    // Utilizamos este ciclo for para
+                    {                                                                           // la segunda cadena.
+                        if (iPalabra1[i] == iPalabra2[j])           // Si hay coincidencia de letras en ambas palabras, se eliminan 
+                        {                                           // de ambas cadenas.
                             iPalabra1 = iPalabra1.Remove(i, 1);
                             iPalabra2 = iPalabra2.Remove(j, 1);
-                            iAnagrama = true;
-                        }
+                            iCoincidencia = true;                   // A la variable iCoincidencia se le asigna true para indicar 
+                        }                                           // que se encontro la letra
                         else
                         {
-                            iAnagrama = false;
-                        }
+                            iCoincidencia = false;                  // A la variable iCoincidencia se le asigna false para indicar 
+                        }                                           // que no se encontro la letra de la primer palabra en la segunda.
                     }
 
-                    if (iAnagrama == false)
+                    if (iCoincidencia == false)
                     {
-                        i++;
-                    }
+                        i++;                                        // Se incrementa el valor de i para pasar a la siguiente 
+                    }                                               // posicion de la primer palabra.
                 }
 
-                if ((iPalabra1.Length == 0) && (iAnagrama == true))
-                {
+                if (iPalabra1.Length == 0)                          // Si se eliminaron todas las letras de la primer palabra 
+                {                                                   // ingresada, significa que son anagramas.
                     Console.WriteLine("Las palabras ingresadas son anagramas.");
                     Console.ReadLine();
                 }
@@ -63,8 +63,8 @@ namespace EJ16
             }
             else
             {
-                Console.WriteLine("Las palabras ingresadas no son anagramas.");
-                Console.ReadLine();
+                Console.WriteLine("Las palabras ingresadas no son anagramas.");     // Si las palabras no tienen el mismo size,
+                Console.ReadLine();                                                 // se muestra en pantalla que no son anagramas.
             }
         }
     }
