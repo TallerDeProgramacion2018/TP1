7bc9530c1243d67d58ab108d3a6c8e5df1fd3a40
Gaston Milano <gastonmilano15@gmail.com>
2018-08-16 23:46:34 -0300

    Finalizado

--- EJ13/Program.cs
+++ EJ13/Program.cs
@@ -10,20 +10,20 @@ namespace EJ13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cadenas invertidas de forma descendente:");
+            Console.WriteLine("Cadenas invertidas y ordenadas de forma de forma descendente:");
             Console.WriteLine();
 
             string palabraInvertida = "";
             string[] invertidas = new string[args.Length];
             int size = 0;
 
-            for (int i = 0; i <= (args.Length - 1); i++)
-            {
+            for (int i = 0; i <= (args.Length - 1); i++)            // Se recorre el array que contiene las palabras ingresadas 
+            {                                                       // en los parametros de la aplicacion.
                 size = args[i].Length - 1;
                 palabraInvertida = "";
 
-                for (; size >=0 ; size--)
-                {
+                for (; size >= 0 ; size--)                          // Se recorre la palabra que contiene la posicion del arreglo 
+                {                                                   // de forma inversa y se concatena cada caracter.
                     palabraInvertida += args[i].Substring(size, 1);
                 }
 
@@ -44,9 +44,9 @@ namespace EJ13
                 }
             }
 
-            for (int i = 0; i < invertidas.Length; i++)
-            {
-                Console.WriteLine(invertidas[i]);
+            for (int i = invertidas.Length - 1; i >= 0; i--)    // Se muestra cada elemento del vector, pero se recorre de
+            {                                                   // de forma inversa para mostrar los elementos de forma descendente.
+                Console.WriteLine(invertidas[i]);               
             }
 
             Console.ReadLine();
